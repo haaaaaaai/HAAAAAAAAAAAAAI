55a9a9c3531398679b984c1055ac5d4d85de981e
haaaaaaai <75528705+haaaaaaai@users.noreply.github.com>
2020-12-05 19:45:54 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,51 @@
+# a=int(input("chislo"))
+# if not a%2:
+#     print("parnoe")
+# elif a%2:
+#     print("-") 
+
+# a=int(input("chislo"))
+# if a<0 or a==0:
+#      print("norm")   
+# else:
+#      print("unnorm")         
+
+
+
+# a=int(input("chislo"))
+# if not a==0:
+#      print("verno")   
+# else:
+#      print("neverno")         
+
+
+# a=input("chislo")
+# if  a=="тест":
+#      print("verno")   
+# else:
+#      print("neverno")         
+
+
+
+# a = 1 
+# if a==1:
+#     print("virno")
+# else:
+#     print ("не верно")
+
+
+
+# a=1
+# b = 4
+# if a==1 or a<1 and b==3 or b>3:
+#     print(a + b)
+# else:
+#     print(a-b)
+
+
+# a=1
+# b=7
+# if a>2 and a<11 or (b>6 or b==6 and b<14):
+#     print ("verno")
+# else:
+#     print ("neverno")
